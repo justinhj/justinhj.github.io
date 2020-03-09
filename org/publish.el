49997998b66b740df6bcf8bec75a538162ef71a8
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-03-09 07:06:25 -0700

    add emacs publish settings and sample post

---
+++ org/publish.el
@@ -0,0 +1,19 @@
+;; -*- flycheck-disabled-checkers: (emacs-lisp-checkdoc); byte-compile-warnings: (not free-vars) -*-
+;; Run this then M-x org-publish
+
+(setq project-root (locate-dominating-file "." "_config.yml"))
+
+(setq org-publish-project-alist
+  `(
+    ("org-justinhj"
+     ;; Path to your org files.
+     :base-directory ,(concat project-root "org/posts")
+     :base-extension "org"
+     ;; Path to your Jekyll project.
+     :publishing-directory ,(concat project-root "_posts")
+     :recursive t
+     :publishing-function org-html-publish-to-html
+     :headline-levels 4
+     :html-extension "html"
+     :body-only t ;; Only export section between <body> </body>
+     )))
