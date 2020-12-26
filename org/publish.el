36d0acebd5fc62bdcddd2e345a2001103c8e8d4e
Justin Heyes-Jones <justinhj@gmail.com>
2020-12-25 20:56:23 -0800

    Fix publish.el cannot be loaded. Fix as

--- org/publish.el
+++ org/publish.el
@@ -1,16 +1,21 @@
 ;; -*- flycheck-disabled-checkers: (emacs-lisp-checkdoc); byte-compile-warnings: (not free-vars) -*-
 ;; Eval this buffer then M-x org-publish
 
-(setq project-root (locate-dominating-file "." "_config.yml"))
+(defun this-file-name()
+  (or load-file-name buffer-file-name (error "What?")))
+
+(message "folder %s" (this-file-name))
+
+(setq github-project-root (locate-dominating-file (this-file-name) "_config.yml"))
 
 (setq org-publish-project-alist
   `(
     ("org-justinhj"
      ;; Path to your org files.
-     :base-directory ,(concat project-root "org/posts")
+     :base-directory ,(concat github-project-root "org/posts")
      :base-extension "org"
      ;; Path to your Jekyll project.
-     :publishing-directory ,(concat project-root "_posts")
+     :publishing-directory ,(concat github-project-root "_posts")
      :recursive t
      :publishing-function org-html-publish-to-html
      :section-numbers nil
@@ -19,9 +24,9 @@
      :body-only t
      )
     ("css"
-     :base-directory ,(concat project-root "org/css")
+     :base-directory ,(concat github-project-root "org/css")
      :base-extension "css"
-     :publishing-directory ,(concat project-root "_orgcss")
+     :publishing-directory ,(concat github-project-root "_orgcss")
      :publishing-function org-publish-attachment
      :recursive nil)
     ("all" :components ("org-justinhj" "css"))
