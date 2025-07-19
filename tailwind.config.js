1a1c12bb294235d270f4f4880302dc45278aef39
Justin Heyes-Jones <justinhj@gmail.com>
2025-07-18 18:44:15 -0700

    add git ignore

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    './*.html',
+    './*.md'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
